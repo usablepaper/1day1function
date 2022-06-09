d62bfa9fd8a1ac74ed66fb236ea6abd3cfccd7ca
usablepaper <usablepaper@gmail.com>
2022-06-09 22:54:48 +0900

    06.09

---
+++ 15_반장선거.js
@@ -0,0 +1,18 @@
+const array = ["원범", "원범", "혜원", "혜원", "혜원", "혜원", "유진", "유진"];
+
+let result = {};
+let winner = "";
+
+for (let index in array) {
+    let val = array[index];
+    result[val] = result[val] === undefined ? 1 : result[val] + 1;
+}
+
+// Object.keys 키값만 배열로 반환
+winner = Object.keys(result).reduce((a, b) => {
+    //console.log(a, b)
+    return result[a] > result[b] ? a : b;
+});
+
+//console.log(winner);
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
